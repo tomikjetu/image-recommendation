b6f88cdaf60558e2619a727e4abc61083aba8329
Tomáš Kordoš <kordo.t12@gmail.com>
2024-12-21 18:19:36 +0100

    like implementation

--- application/routes/api/interaction_like.py
+++ application/routes/api/interaction_like.py
@@ -1,1 +1,21 @@
-#implementation 1
+from application.app import app
+from flask import request
+from application.storage.storage_manager import users, save_json, users_file
+from application.storage.user import User
+
+@app.route('/like', methods=['POST'])
+def like():
+    data = request.get_json()
+    user_id = data.get('user_id')
+    post_id = data.get('post_id')
+    
+    if not user_id or not post_id:
+        return {'error': 'Invalid request'}, 400
+
+    if user_id not in users:
+        users[user_id] = User(user_id)
+    user = users[user_id]
+    user.like(post_id)
+    save_json(users_file, users)
+
+    return {'message': 'Post liked'}, 200
