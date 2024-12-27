ce4a5371d642644fe9b431c014d5083e6bf8b540
Tomáš Kordoš <kordo.t12@gmail.com>
2024-12-27 19:15:07 +0100

    user embedding function

--- embedding/user_embedding.py
+++ embedding/user_embedding.py
@@ -1,9 +1,14 @@
-# implementation 2
 from models_manager import get_sentence_transformer_model
+import numpy as np
 
-def embed_user(user):     
-    model = get_sentence_transformer_model()
-    description = f"{post['description']}"
+from application.storage.storage_manager import users, p_embeddings
 
-    embedding = model.encode(description)
-    return embedding.tolist() 
+def embed_user(user_id):     
+    user = users[user_id]
+    liked_posts = user["liked_posts"]
+    if(len(liked_posts)==0):
+        return []
+
+    liked_embeddings = [p_embeddings[post_id] for post_id in liked_posts]
+    combined_embedding = np.mean(liked_embeddings, axis=0).reshape(1, -1)
+    return combined_embedding
--- recommendation/similar_post.py
+++ recommendation/similar_post.py
@@ -1,23 +1,20 @@
-import json
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
 from application.storage.storage_manager import users, p_embeddings
+from embedding.user_embedding import embed_user
 
 def similar_posts(user_id, top_n=5):
     user = users[user_id]
-    liked_posts = user["liked_posts"]
     seen_posts = user["has_seen"]
-    if(len(liked_posts)==0):
+    u_embedding = embed_user(user_id)
+    if(len(u_embedding)==0):
         return []
-
-    liked_embeddings = [p_embeddings[post_id] for post_id in liked_posts]
-    combined_embedding = np.mean(liked_embeddings, axis=0).reshape(1, -1)
 
     candidate_ids = [post_id for post_id in p_embeddings if post_id not in seen_posts]
     candidate_embeddings = [p_embeddings[post_id] for post_id in p_embeddings if post_id not in seen_posts]    
 
-    similarities = cosine_similarity(combined_embedding, candidate_embeddings)[0]
+    similarities = cosine_similarity(u_embedding, candidate_embeddings)[0]
     post_similarity_pairs = list(zip(candidate_ids, similarities))
     sorted_pairs = sorted(post_similarity_pairs, key=lambda x: x[1], reverse=True)
 
